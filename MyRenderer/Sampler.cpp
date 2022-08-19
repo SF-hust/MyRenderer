c4f9bd87c01119dd71e97089e5dfe43d1f30c0ec
zhaoyi.577 <zhaoyi.577@bytedance.com>
2022-08-19 17:42:36 +0800

    refector sampler, not complete

--- MyRenderer/Sampler.cpp
+++ MyRenderer/Sampler.cpp
@@ -3,17 +3,27 @@
 template <class T>
 T Sampler2D<T>::sample(const Texture2D<T> &tex, Vec2f uv, Vec2f ddxUV, Vec2f ddyUV, const PipelineState &pipelineState) const
 {
+    // clamp to border
+    if(addressMode == ADDRESS_MODE_CLAMP_TO_BORDER && (uv.u > 0.0f || uv.u < 1.0f || uv.v < 0.0f || uv.v > 1.0f))
+    {
+        return borderColor;
+    }
+    if(filterMode == FILTER_MODE_ANISOTROPIC)
+    {
+        return sampleAnistrophic(tex, uv, ddxUV, ddyUV);
+    }
+    Vec2f sampleUV = getSampleUV(uv);
     T result = {};
     switch (mipmapMode)
     {
     case MIPMAP_MODE_NO_MIPMAP:
-        result = sampleFromUVLevel(tex, uv, 0, 0);
+        result = sampleFromMipmapLevel(tex, sampleUV, 0, 0);
         break;
     case MIPMAP_MODE_NEAREST:
         float scale = std::max(ddxUV.x * (float)tex.width, ddyUV.y * (float)tex.height);
         int mip = fmodf(scale, 1.0f) > 0.5f ? (int)scale : (int)scale - 1;
         mip = std::min(std::max(mip, 0), tex.maxMipmapLevel);
-        result = sampleFromUVLevel(tex, uv, mip, mip);
+        result = sampleFromMipmapLevel(tex, uv, mip, mip);
         break;
     case MIPMAP_MODE_LINEAR:
     {
@@ -21,39 +31,24 @@ T Sampler2D<T>::sample(const Texture2D<T> &tex, Vec2f uv, Vec2f ddxUV, Vec2f ddy
         float factor = fmodf(scale, 1.0f);
         int mip1 = std::min(std::max((int)scale - 1, 0), tex.maxMipmapLevel);
         int mip2 = std::min(std::max((int)scale, 0), tex.maxMipmapLevel);
-        result += (1.0f - factor) * sampleFromUVLevel(tex, uv, mip1, mip1);
-        result += factor * sampleFromUVLevel(tex, uv, mip2, mip2);
+        result += (1.0f - factor) * sampleFromMipmapLevel(tex, sampleUV, mip1, mip1);
+        result += factor * sampleFromMipmapLevel(tex, sampleUV, mip2, mip2);
     }
     break;
     }
     return result;
 }
+
 template <class T>
-T Sampler2D<T>::sampleFromUVLevel(const Texture2D<T>& tex, Vec2f rawUV, int ul, int vl) const
+T Sampler2D<T>::sampleFromMipmapLevel(const Texture2D<T>& tex, Vec2f uv, int mipmapLevel) const
 {
-    Vec2f uv;
-    T result;
-    switch (addressMode)
+    switch (filterMode)
     {
-    case ADDRESS_MODE_REPEAT:
-        uv.u = rawUV.u >= 0.0f ? fmod(rawUV.u, 1.0f) : fmod(rawUV.u, 1.0f) + 1.0f;
-        uv.v = rawUV.v >= 0.0f ? fmod(rawUV.v, 1.0f) : fmod(rawUV.v, 1.0f) + 1.0f;
-        break;
-    case ADDRESS_MODE_MIRRORED_REPEAT:
-        uv.u = 1.0f - std::abs(fmod(std::abs(rawUV.u), 2.0f) - 1.0f);
-        uv.u = 1.0f - std::abs(fmod(std::abs(rawUV.v), 2.0f) - 1.0f);
-        break;
-    case ADDRESS_MODE_CLAMP_TO_EDGE:
-        uv.u = clamp(rawUV.u, 0.0f, 1.0f);
-        uv.v = clamp(rawUV.v, 0.0f, 1.0f);
-        break;
-    case ADDRESS_MODE_CLAMP_TO_BORDER:
-        if (rawUV.u > 1.0f || rawUV.u < 0.0f || rawUV.v > 1.0f || rawUV.v < 0.0f)
-        {
-            return borderColor;
-        }
-        uv = rawUV;
-        break;
+    case FILTER_MODE_LINEAR:
+        return samplePoint(tex, uv, mipmapLevel);
+    case FILTER_MODE_POINT:
+    default:
+        return sampleLinear(tex, uv, mipmapLevel);
     }
 
     int stx = 0, edx = tex.width, sty = 0, edy = tex.height;
@@ -168,4 +163,121 @@ T Sampler2D<T>::sampleFromUVLevel(const Texture2D<T>& tex, Vec2f rawUV, int ul, 
     break;
     }
     return result;
+}
+
+template <class T>
+T Sampler2D<T>::samplePoint(const Texture2D<T>& tex, Vec2f uv, int mipmapLevel) const
+{
+    int w = (int)tex.width >> mipmapLevel;
+    int h = (int)tex.height >> mipmapLevel;
+    int x = std::min((int)(uv.u * w), w - 1);
+    int y = std::min((int)(uv.v * h), h - 1);
+    return tex.getMipmapped(x, y, mipmapLevel);
+}
+
+template <class T>
+T Sampler2D<T>::sampleLinear(const Texture2D<T>& tex, Vec2f uv, int mipmapLevel) const
+{
+    int w = (int)tex.width >> mipmapLevel;
+    int h = (int)tex.height >> mipmapLevel;
+    float xf = uv.u * (float)w;
+    float yf = uv.v * (float)h;
+    int x0, x1, y0, y1;
+    float ku, kv;
+    // calculate 4 pixel coords and 2 factors to blend
+    if (uv.u == 1.0f)
+    // if address mode is ADDRESS_MODE_REPEAT, uv.u can't be 1.0f
+    {
+        ku = 1.0f;
+        x0 = x1 = w - 1;
+    }
+    else if (fmodf(xf, 1.0f) < 0.5f)
+    // sample point in left half of texel
+    {
+        ku = 0.5f - fmodf(xf, 1.0f);
+        x1 = int(xf);
+        if (addressMode == ADDRESS_MODE_REPEAT && x1 == stx)
+        // when x1 is the first left and address mode is ADDRESS_MODE_REPEAT, the x0 should be the first right
+        {
+            x0 = w - 1;
+        }
+        else
+        {
+            x0 = std::max(x1 - 1, 0);
+        }
+    }
+    else
+    // sample point in right half of texel
+    {
+        ku = 1.5f - fmodf(uvInTex.u, 1.0f);
+        x0 = int(uvInTex.u);
+        if (addressMode == ADDRESS_MODE_REPEAT && x0 == edx - 1)
+        // when x0 is the first right and address mode is ADDRESS_MODE_REPEAT, the x1 should be the first left
+        {
+            x1 = 0;
+        }
+        else
+        {
+            x1 = std::min(x0 + 1, int(w - 1));
+        }
+    }
+
+    if (uv.v == 1.0f)
+    // if address mode is ADDRESS_MODE_REPEAT, uv.v can't be 1.0f
+    {
+        kv = 1.0f;
+        y0 = y1 = edy - 1;
+    }
+    else if (fmodf(uvInTex.v, 1.0f) < 0.5f)
+    // bottom half
+    {
+        kv = 0.5f - fmodf(uvInTex.v, 1.0f);
+        y1 = int(uvInTex.v);
+        if (addressMode == ADDRESS_MODE_REPEAT && y1 == sty)
+        // when y1 is the first bottom and address mode is ADDRESS_MODE_REPEAT, the y0 should be the first top
+        {
+            y0 = edy - 1;
+        }
+        else
+        {
+            y0 = std::max(y1 - 1, sty);
+        }
+    }
+    else
+    // top half
+    {
+        kv = 1.5f - fmodf(uvInTex.v, 1.0f);
+        y0 = int(uvInTex.v);
+        if (addressMode == ADDRESS_MODE_REPEAT && y0 == edy - 1)
+        // when y0 is the first top and address mode is ADDRESS_MODE_REPEAT, the y1 should be the first bottom
+        {
+            y1 = sty;
+        }
+        else
+        {
+            y1 = std::min(y0 + 1, int(edy - 1));
+        }
+    }
+}
+
+template <class T>
+Vec2f Sampler2D<T>::getSampleUV(Vec2f rawUV) const
+{
+    Vec2f uv = {};
+    switch (addressMode)
+    {
+    case ADDRESS_MODE_REPEAT:
+        uv.u = rawUV.u >= 0.0f ? fmod(rawUV.u, 1.0f) : fmod(rawUV.u, 1.0f) + 1.0f;
+        uv.v = rawUV.v >= 0.0f ? fmod(rawUV.v, 1.0f) : fmod(rawUV.v, 1.0f) + 1.0f;
+        break;
+    case ADDRESS_MODE_MIRRORED_REPEAT:
+        uv.u = 1.0f - std::abs(fmod(std::abs(rawUV.u), 2.0f) - 1.0f);
+        uv.u = 1.0f - std::abs(fmod(std::abs(rawUV.v), 2.0f) - 1.0f);
+        break;
+    case ADDRESS_MODE_CLAMP_TO_EDGE:
+        uv.u = clamp(rawUV.u, 0.0f, 1.0f);
+        uv.v = clamp(rawUV.v, 0.0f, 1.0f);
+        break;
+    }
+    return uv;
 }
--- MyRenderer/Sampler.h
+++ MyRenderer/Sampler.h
@@ -45,14 +45,16 @@ public:
     void setBorderColor(Vec4f b) { borderColor = b; }
 
 protected:
-    T sampleFromUVLevel(const Texture2D<T>& tex, Vec2f rawUV, int ul, int vl) const;
+    T sampleFromMipmapLevel(const Texture2D<T>& tex, Vec2f uv, int mipmapLevel) const;
 
     T sampleAnistrophic(const Texture2D<T>& tex, Vec2f rawUV, Vec2f ddxUV, Vec2f ddyUV) const;
 
-    T samplePoint(const Texture2D<T>& tex, Vec2f rawUV, int mipmapLevel) const;
+    T samplePoint(const Texture2D<T>& tex, Vec2f uv, int mipmapLevel) const;
 
-    T sampleLinear(const Texture2D<T>& tex, Vec2f rawUV, int mipmapLevel) const;
+    T sampleLinear(const Texture2D<T>& tex, Vec2f uv, int mipmapLevel) const;
     
+    Vec2f getSampleUV(Vec2f rawUV) const;
+
 protected:
     AddressMode addressMode = ADDRESS_MODE_CLAMP_TO_BORDER;
     MipMapMode mipmapMode = MIPMAP_MODE_NO_MIPMAP;
